10df4f0118d9bb08c79333f23e725aae6e76e6ae
Martin Dimitrov <martin3dimitrov@gmail.com>
2020-02-22 13:25:53 -0500

    adding paths

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Dashboard</h1>
+    <div>
+      <BrowserRouter>
+        <Route path='/groups' component={()=><h1>Groups</h1>}/>
+        <Route path='/events' component={()=><h1>Events</h1>} />
+      </BrowserRouter>
+    </div>
   )
 }
 
