19ba2d3a0a68a48e181bb6aa34741479da017588
Martin Dimitrov <martin3dimitrov@gmail.com>
2020-07-30 19:58:20 -0400

    added nx monorepo

---
+++ floppd/libs/ui/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/event/event';
+export * from './lib/table/table';
+export * from './lib/page/page';
---
+++ floppd/libs/ui/src/lib/event/event.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Event from './event';
+
+describe(' Event', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Event />);
+    expect(baseElement).toBeTruthy();
+  });
+});
