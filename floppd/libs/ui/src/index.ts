66dd674caf3120bd76356e99198f9ed209de5f1c
Martin Dimitrov <martin3dimitrov@gmail.com>
2020-08-04 17:04:17 -0400

    cleanup

--- floppd/libs/ui/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './lib/darkmode-trigger/darkmode-trigger';
-export * from './lib/error/error-component';
-export * from './lib/name-picker/name-picker';
-export * from './lib/event-creator/event-creator';
-export * from './lib/event/event';
-export * from './lib/table/table';
-export * from './lib/page/page';
